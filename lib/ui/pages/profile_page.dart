01aa819704b9000384058533d4e747d3b63804c2
ZenMasterJacob20011 <jsnichols0404@hotmail.com>
2023-04-26 21:36:27 -0500

    Added admin view and customer view

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -1,9 +1,13 @@
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mbs/custom_icons_icons.dart';
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+  String name;
+  String email;
+
+  ProfilePage({super.key, required this.name, required this.email});
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
@@ -43,12 +47,12 @@ class _ProfilePageState extends State<ProfilePage> {
             color: Color(0XFFb2a4c7),
             thickness: 3,
           ),
-          profilePicAndName,
+          ProfilePicAndName(name: widget.name, email: widget.email),
           const Divider(
             color: Color(0XFFb2a4c7),
             thickness: 3,
           ),
-          manageAccount,
+          ManageAccount(name: widget.name, email: widget.email),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -78,137 +82,201 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 }
 
-Widget profilePicAndName = Row(
-  mainAxisAlignment: MainAxisAlignment.center,
-  children: [
-    const Icon(Icons.person, size: 100, color: Colors.white),
-    Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(bottom: 8.0),
-          child: Text(
-            "Hello,\n" + "Tom!",
-            style: GoogleFonts.poppins(
-              textStyle: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontSize: 35),
-            ),
-          ),
-        ),
-        Text('tbrown@ttu.edu',
-            style: GoogleFonts.poppins(
-                textStyle: const TextStyle(
-              color: Colors.white,
-              fontSize: 25,
-            ))),
-      ],
-    )
-  ],
-);
+class ProfilePicAndName extends StatelessWidget {
+  String name;
+  String email;
 
-Widget manageAccount = Expanded(
-    child: Column(
-  mainAxisAlignment: MainAxisAlignment.center,
-  crossAxisAlignment: CrossAxisAlignment.center,
-  children: [
-    Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Text(
-        'Manage account',
-        style: GoogleFonts.poppins(
-            textStyle: const TextStyle(fontSize: 25),
-            fontWeight: FontWeight.w700,
-            color: Colors.white),
-      ),
-    ),
-    Padding(
-      padding: const EdgeInsets.all(12),
-      child: Card(
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10))),
-        color: const Color(0XFF694f93),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+  ProfilePicAndName({Key? key, required this.name, required this.email})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Icon(Icons.person, size: 100, color: Colors.white),
+        Column(
           children: [
             Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(right: 4.0),
-                    child: Icon(Icons.person, color: Colors.white),
-                  ),
-                  Text(
-                    'Personal information',
-                    style: GoogleFonts.poppins(
-                      textStyle:
-                          const TextStyle(color: Colors.white, fontSize: 20),
-                    ),
-                  ),
-                ],
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Text(
+                "Hello,\n$name",
+                style: GoogleFonts.poppins(
+                  textStyle: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 35),
+                ),
               ),
             ),
-            const Divider(
-              color: Color(0XFFb2a4c7),
-              thickness: 3,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(right: 4.0),
-                    child: Icon(Icons.video_camera_back, color: Colors.white),
-                  ),
-                  Text(
-                    'My Cinema',
-                    style: GoogleFonts.poppins(
-                      textStyle:
-                          const TextStyle(color: Colors.white, fontSize: 20),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const Divider(
-              color: Color(0XFFb2a4c7),
-              thickness: 3,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(right: 4.0),
-                    child: Icon(Icons.payment, color: Colors.white),
-                  ),
-                  Text(
-                    'Saved Payment Accounts',
-                    style: GoogleFonts.poppins(
-                      textStyle:
-                          const TextStyle(color: Colors.white, fontSize: 20),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            Text(email,
+                style: GoogleFonts.poppins(
+                    textStyle: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                ))),
           ],
-        ),
-      ),
-    ),
-    Padding(
-      padding: const EdgeInsets.only(top: 40),
-      child: Row(
+        )
+      ],
+    );
+  }
+}
+
+class ManageAccount extends StatelessWidget {
+  String name;
+  String email;
+
+  ManageAccount({Key? key, required this.name, required this.email})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Icon(Icons.exit_to_app_rounded, color: Colors.white, size: 50),
-          Text('Sign out', style: TextStyle(color: Colors.white, fontSize: 30))
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Manage account',
+            style: GoogleFonts.poppins(
+                textStyle: const TextStyle(fontSize: 25),
+                fontWeight: FontWeight.w700,
+                color: Colors.white),
+          ),
+          name == 'admin' ? adminCard : personCard,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Icon(Icons.exit_to_app_rounded, color: Colors.white, size: 50),
+              Text('Sign out',
+                  style: TextStyle(color: Colors.white, fontSize: 30))
+            ],
+          ),
+        ].map((widget) => Padding(
+          padding: EdgeInsets.all(8),
+          child: widget,
+        ))
+            .toList(),
+      ),
+    );
+  }
+}
+
+Widget adminCard = Card(
+  shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(10))),
+  color: const Color(0XFF694f93),
+  child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(right: 4.0),
+            child: Icon(Icons.person, color: Colors.white),
+          ),
+          Text(
+            'Personal information',
+            style: GoogleFonts.poppins(
+              textStyle: const TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
         ],
       ),
-    ),
-  ],
-));
+      const Divider(
+        color: Color(0XFFb2a4c7),
+        thickness: 3,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(right: 4.0),
+            child: Icon(Icons.print, color: Colors.white),
+          ),
+          Text(
+            'Print Report',
+            style: GoogleFonts.poppins(
+              textStyle: const TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
+        ],
+      )
+    ]
+        .map((widget) => Padding(
+              padding: EdgeInsets.all(8),
+              child: widget,
+            ))
+        .toList(),
+  ),
+);
+
+Widget personCard = Card(
+  shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(10))),
+  color: const Color(0XFF694f93),
+  child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(right: 4.0),
+            child: Icon(Icons.person, color: Colors.white),
+          ),
+          Text(
+            'Personal information',
+            style: GoogleFonts.poppins(
+              textStyle: const TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
+        ],
+      ),
+      const Divider(
+        color: Color(0XFFb2a4c7),
+        thickness: 3,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(right: 4.0),
+            child: Icon(Icons.video_camera_back, color: Colors.white),
+          ),
+          Text(
+            'My cinema',
+            style: GoogleFonts.poppins(
+              textStyle: const TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
+        ],
+      ),
+      const Divider(
+        color: Color(0XFFb2a4c7),
+        thickness: 3,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(right: 4.0),
+            child: Icon(Icons.payment, color: Colors.white),
+          ),
+          Text(
+            'Saved payment accounts',
+            style: GoogleFonts.poppins(
+              textStyle: const TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
+        ],
+      )
+    ]
+        .map((widget) => Padding(
+              padding: EdgeInsets.all(8),
+              child: widget,
+            ))
+        .toList(),
+  ),
+);
